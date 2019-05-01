4dd28cbf9abd297029b216133a0385d5d84b991c
Elise Pham <elise.pham88@gmail.com>
2019-05-01 15:15:49 -0700

    added relationship between review and product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_01_220207) do
+ActiveRecord::Schema.define(version: 2019_05_01_220933) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2019_05_01_220207) do
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "product_id"
+    t.index ["product_id"], name: "index_reviews_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema.define(version: 2019_05_01_220207) do
   end
 
   add_foreign_key "products", "users"
+  add_foreign_key "reviews", "products"
 end
