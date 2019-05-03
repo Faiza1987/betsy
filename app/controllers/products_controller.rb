8423b5c7be6ec854f98edc60ded15bd4e5c68f06
laneia <laneiamurray@gmail.com>
2019-05-03 13:15:52 -0400

    add index method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,15 @@
 class ProductsController < ApplicationController
+  def index
+    if params[:category]
+      @title = "#{params[:category].upcase}"
+      @products = Product.where(:category => params[:category])
+      @products = @products.order(:name)
+    else
+      @title = "All Products"
+      @products = Product.all.order(:name)
+    end
+  end
+
   private
 
   def product_params
