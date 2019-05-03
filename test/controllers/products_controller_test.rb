165ddd4f46ee1a2ebf72864fb555552083bcb1f2
laneia <laneiamurray@gmail.com>
2019-05-03 16:24:57 -0400

    added index and new products controller tests

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe ProductsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:bad_product_id) { Product.first.destroy.id }
+
+  describe "index" do
+    it "should get index" do
+      get products_path
+      must_respond_with :success
+    end
+  end
+
+  describe "new" do
+    it "can get the new page" do
+      get new_product_path
+      must_respond_with :success
+    end
+  end
 end
