8139a1cfede97035fe4224cf481e12e7fa118930
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-03 15:27:28 -0700

    order items controller actions and tests created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action: find_user
+  before_action :find_user
 
   def find_user
     @current_user = User.find_by(id: session[:user_id])
