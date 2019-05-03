6a8426626be6bf7ddfa3e90a3f20dbb918b18c5d
laneia <laneiamurray@gmail.com>
2019-05-03 12:53:14 -0400

    added product_params private method to products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,7 @@
 class ProductsController < ApplicationController
+  private
+
+  def product_params
+    return params.require(:product, :name).permit(:category, :price, :stock, :user_id)
+  end
 end
