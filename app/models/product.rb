72d4c0b4f194f211f69b808fe8bcfb50d9c8e1e1
laneia <laneiamurray@gmail.com>
2019-05-03 12:09:21 -0400

    added validation to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,11 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
+  validates :price, presence: true, numericality: {:greater_than => 0}
+  validates :stock, presence: true, numericality: {:only_integer => true, :greater_than_or_equal_to => 0}
+
   has_and_belongs_to_many :categories
   belongs_to :user
   has_many :reviews
-
   has_many :orderitems
   has_many :orders, :through => orderitems
 end
