344c0d56280fd4b330617142b5884ca73e76fc12
laneia <laneiamurray@gmail.com>
2019-05-03 14:10:42 -0400

    added edit, index, new product html pages

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -88,7 +88,7 @@ class ProductsController < ApplicationController
   private
 
   def product_params
-    return params.require(:product, :name).permit(:category, :price, :stock, :user_id)
+    return params.require(:product, :name).permit(:category, :price, :stock, :user_id, :orderitem_id)
   end
 
   def product_merchant?
