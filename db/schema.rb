cd820c4bd02f1e96214b7cc930597c30bba9d775
Elise Pham <elise.pham88@gmail.com>
2019-05-06 16:52:02 -0700

    added photo_url column to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_04_213058) do
+ActiveRecord::Schema.define(version: 2019_05_06_235031) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2019_05_04_213058) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "photo_url"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
