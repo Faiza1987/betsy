7272c7d9f20a9f2d4a1e784f3be4cca6e9545f92
Faiza Husain <faiza.ahsan1222@gmail.com>
2019-05-06 17:02:48 -0700

    Added validation tests for users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :products
 
-  validates :username, uniqueness: true, presence: true
-  validates :email, presence: true
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
 
   def self.build_from_github(auth_hash)
     return User.new(uid: auth_hash[:uid], provider: "github", email: auth_hash["info"]["email"], username: auth_hash["info"]["name"])
