7b75675100948414bec62dd5dfc22960b5fd42e4
laneia <laneiamurray@gmail.com>
2019-05-07 12:53:59 -0400

    added test for create new product w/ valid data

--- test/controllers/reviews_controller_test.rb
+++ test/controllers/reviews_controller_test.rb
@@ -4,9 +4,25 @@ describe ReviewsController do
   let(:product) { products(:chair) }
 
   describe "new" do
-    it "succeeds" do
+    it "can create a new review" do
       get new_product_review_path(product.id)
       must_respond_with :success
+    end
+  end
+
+  describe "create" do
+    it "creates a new review for product given valid data" do
+      proc {
+        post product_reviews_path(product.id), params: {
+                                                 review: {
+                                                   rating: 1,
+                                                   description: "Worst chair ever.",
+                                                 },
+                                               }
+      }.must_change "Review.count", +1
+
+      must_respond_with :redirect
+      must_redirect_to product_path(product.id)
     end
   end
 end
