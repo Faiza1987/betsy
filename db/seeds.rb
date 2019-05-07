de5793cc2bda3ce9f9eb6c1729cbbe3856282e42
laneia <laneiamurray@gmail.com>
2019-05-07 09:25:53 -0400

    added users to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = [
+    ["laneia", "laneia@email.com"],
+    ["faiza", "faiza@email.com"],
+    ["amyw", "amy@email.com"],
+    ["elise", "elise@email.com"],
+    ["tildee", "tildee@email.com"],
+  ]
+  
+  users.each do |username, email|
+    User.create(
+      username: username,
+      email: email,
+    )
+  end
+  
+  
