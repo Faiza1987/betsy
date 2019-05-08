0609958aba9206737df04af4cca3ec449187abc9
laneia <laneiamurray@gmail.com>
2019-05-08 11:28:06 -0400

    added order model validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   has_many :orderitems
   has_many :products, :through => :orderitems
+
+  validates :name, presence: true, if: :paid?
+  validates :email, presence: true, if: :paid?
+  validates :mailing_address, presence: true, if: :paid?
+  validates :credit_card_num, presence: true, length: {in: 16..16}, if: :paid?
+  validates :card_expiration_date, presence: true, if: :paid?
+  validates :cvv, presence: true, if: :paid?
+  validates :billing_zip_code, presence: true, length: {in: 5..5}, if: :paid?
 end
