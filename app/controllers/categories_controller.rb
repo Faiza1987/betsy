e3a3e91d6d73984b69e26f3d54226740777a3d57
laneia <laneiamurray@gmail.com>
2019-05-08 09:19:10 -0400

    added categories controller code, tweaking product show & seeds

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,24 @@
 class CategoriesController < ApplicationController
+  before_action :require_login, only: [:new, :create]
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Category added!"
+      redirect_to categories_path
+    else
+      flash.now[:error] = @category.errors
+      render :new, status: :error
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:category_name, :category_id, product_ids: [])
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -41,7 +41,7 @@ Product.create!(name: "Bag of Dicks",
                 description: "Eat it.",
                 photo_url: "https://i.imgur.com/eOJJDuE.jpg",
                 user_id: rand(1..5),
-                category_ids: [1, 5])
+                category_ids: ["NSFW", "Edible"])
 
 Product.create!(name: "Glitter Bomb",
                 price: 5,
@@ -49,7 +49,7 @@ Product.create!(name: "Glitter Bomb",
                 description: "Show them how much you care with the gift that keeps on giving, long after they wish it wouldn't.",
                 photo_url: "https://i.imgur.com/iWsCjox.jpg",
                 user_id: rand(1..5),
-                category_ids: [2, 3])
+                category_ids: ["SFW", "Messy"])
 
 Product.create!(name: "Drop a Deuce (in their mailbox)",
                 price: 25,
@@ -57,7 +57,7 @@ Product.create!(name: "Drop a Deuce (in their mailbox)",
                 description: "When you just can't give a shit anymore, we can do it for you.",
                 photo_url: "https://i.imgur.com/p6As6x8.jpg",
                 user_id: rand(1..5),
-                category_ids: [2, 3])
+                category_ids: ["SFW", "Messy"])
 
 Product.create!(name: "Forbidden Lollipops",
                 price: 5,
@@ -65,7 +65,7 @@ Product.create!(name: "Forbidden Lollipops",
                 description: "Hand dipped in the finest fruity wax candles.",
                 photo_url: "https://i.imgur.com/N0Gyp1f.jpg",
                 user_id: rand(1..5),
-                category_ids: [2])
+                category_ids: ["SFW"])
 
 Product.create!(name: "Chocolate Anaconda",
                 price: 5,
@@ -73,4 +73,4 @@ Product.create!(name: "Chocolate Anaconda",
                 description: "Guaranteed to taste delicious even if you don't got buns, hun.",
                 photo_url: "https://i.imgur.com/xP3MtfZ.jpg",
                 user_id: rand(1..5),
-                category_ids: [1, 5])
+                category_ids: ["NSFW", "Edible"])
