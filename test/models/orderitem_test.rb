795c1e89366de21c9357d0fbd4b5582eac87b2d5
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-09 15:54:26 -0700

    added tests for model custom methods

--- test/models/orderitem_test.rb
+++ test/models/orderitem_test.rb
@@ -35,18 +35,35 @@ describe Orderitem do
 
   describe "custom quantity invalid method" do
     it "will return false if quantity is less than stock" do
+      expect(orderitems(:trick).is_quantity_invalid?).must_equal false
     end
 
     it "will return false if quantity is equal to stock" do
+      expect(orderitems(:halloween).is_quantity_invalid?).must_equal false
     end
 
     it "will return true if quantity larger than stock" do
+      expect(orderitems(:pumpkin).is_quantity_invalid?).must_equal true
     end
   end
 
   describe "custom calculate cost method" do
+    it "will return total cost for quantity larger than one" do
+      expect(orderitems(:treat).calculate_cost).must_equal 5000
+    end
+
+    it "will return total cost for quantity one" do
+      expect(orderitems(:trick).calculate_cost).must_equal 1000
+    end
   end
 
   describe "custom add quantity method" do
+    it "will add additional quantity to existing quantity (1)" do
+      expect(orderitems(:trick).add_quantity(24)).must_equal 25
+    end
+
+    it "will add additional quantity to existing quantity (more than 1)" do
+      expect(orderitems(:treat).add_quantity(23)).must_equal 25
+    end
   end
 end
